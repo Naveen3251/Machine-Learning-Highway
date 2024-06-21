49f10631815e7da75c685550c5a08b6ff562e0f5
Naveen Kumar <114800360+Naveen3251@users.noreply.github.com>
2024-06-21 10:31:47 +0530

    Update Destructor.py

--- Python-OOPs/Destructor.py
+++ Python-OOPs/Destructor.py
@@ -1,5 +1,11 @@
 #Destructor
-
+'''Why Destructor Might Be Called Immediately
+Reference Counting:
+Python uses reference counting to manage memory. An object is destroyed (and __del__ is called) when its reference count drops to zero.
+Interactive Environment:
+In an interactive environment, temporary references to objects might be handled differently. 
+For example, the Python shell or Jupyter notebook might clean up objects immediately after their use to free up memory.
+'''
 class Destroy:
     def __init__(self,name):
         self.name=name
