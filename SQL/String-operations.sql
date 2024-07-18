ce0ef653cadae5f007f85f72ad96ebaa6ee60c09
Naveen Kumar <114800360+Naveen3251@users.noreply.github.com>
2024-07-18 20:01:53 +0530

    Update String-operations.sql

--- SQL/String-operations.sql
+++ SQL/String-operations.sql
@@ -19,11 +19,27 @@ SELECT LOWER(first_name) AS f_name FROM employee;
 
 /*TRIM: 
 Remove leading and trailing spaces
+RTRIM:
+Remove Trailing spaces
+LTRIM
+Remove Leading spaces
 */
 SELECT TRIM(first_name) as f_name FROM employee;
+SELECT RTRIM('NAVEEN   ') AS right_trimmed;
+SELECT LTRIM('    NAVEEN') AS left_trimmed;
 
 /*Length*/
 SELECT LENGTH(first_name) AS length_of_fname FROM employee;
 
 /*Replace*/
 SELECT REPLACE(first_name,'John','Jonathan') AS f_name FROM employee;
+
+
+/*
+LEFT(): Extracts a specified number of characters from the left side of a string.
+RIGHT(): Extracts a specified number of characters from the right side of a string.
+*/
+SELECT LEFT('EXAMPLE',3) AS first_three,RIGHT('EXAMPLE',3) AS  last_three;
+
+/*POSITION(): Returns the position of the first occurrence of a substring in a string.*/
+SELECT POSITION('AMP' IN 'EXAMPLE') AS A, POSITION('AME' IN 'EXAMPLE') as B;
